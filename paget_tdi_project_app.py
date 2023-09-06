6e57f4b9b977add25902521c46c39911315c9e33
aaroncpaget <134789514+aaroncpaget@users.noreply.github.com>
2023-09-06 14:51:02 -0600

    Update paget_tdi_project_app.py
    updated max value on slider

--- paget_tdi_project_app.py
+++ paget_tdi_project_app.py
@@ -25,7 +25,7 @@ def main():
    
     # Sidebar input for a string
     user_input1 = st.sidebar.text_input('Enter an address:', value='1600 Pennsylvania Avenue NW, Washington, DC 20500')
-    user_input4 = st.sidebar.slider('Distance from address in miles:', min_value=5, max_value=200, value=10, step=5, format='%i')
+    user_input4 = st.sidebar.slider('Distance from address in miles:', min_value=5, max_value=100, value=10, step=5, format='%i')
     user_input2 = st.sidebar.number_input('Start Year: (min=1955)', min_value=1955, step=1, value=1996)
     user_input3 = st.sidebar.number_input('End Year: (max=2022)', min_value=user_input2, step=1, value=2022)
 
@@ -36,7 +36,7 @@ def main():
     # Create Matplotlib-based plot
     climate_plot = sa_climate.show_address_climate(user_input1)
     st.markdown(f"### Climate Plot for {user_input1}")
-    st.pyplot(climate_plot.figure)
+    st.pyplot(climate_plot)
 
     # Create Folium map
 #    folium_map = severe_weather_map(user_input1,user_input4,user_input2, user_input3)
